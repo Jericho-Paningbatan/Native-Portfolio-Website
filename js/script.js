8643d9015c66702e06122aef8d813f2febf347e6
Jericho Paningbatan <echoop058@gmail.com>
2024-01-19 23:42:32 +0800

    UPDATE: 11:42 PM

--- js/script.js
+++ js/script.js
@@ -48,3 +48,88 @@ document.body.removeChild(a);
 
 
 
+
+function isElementInViewport(element) {
+  var elementRect = element.getBoundingClientRect();
+  return (
+    elementRect.top >= 0 &&
+    elementRect.left >= 0 &&
+    elementRect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    elementRect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function addhtml() {
+  var myElement = document.getElementById('graph1');
+
+  if (isElementInViewport(myElement)) {
+    myElement.classList.add('html-scrolljs');
+
+  } else {
+    myElement.classList.remove('html-scrolljs');
+
+
+  }
+}
+
+
+function addcss() {
+  let cssElement = document.getElementById('graph2');
+
+  if (isElementInViewport(cssElement)) {
+    cssElement.classList.add('css-scrolljs');
+
+  } else {
+    cssElement.classList.remove('css-scrolljs');
+
+  }
+}
+
+
+function addjs() {
+  let jsElement = document.getElementById('graph3');
+
+  if (isElementInViewport(jsElement)) {
+    jsElement.classList.add('js-scrolljs');
+
+  } else {
+    jsElement.classList.remove('js-scrolljs');
+
+  }
+}
+
+function addreact() {
+  let reactElement = document.getElementById('graph4');
+
+  if (isElementInViewport(reactElement)) {  
+    reactElement.classList.add('react-scrolljs');
+  } else {
+    reactElement.classList.remove('react-scrolljs');
+  }
+}
+
+
+function addbootsrap() {
+  let btElement = document.getElementById('graph5');
+
+  if (isElementInViewport(btElement)) {
+    btElement.classList.add('bootsrap-scrolljs');
+
+  } else {
+    btElement.classList.remove('bootsrap-scrolljs');
+
+  }
+}
+
+
+
+ function handleScroll() {
+  addhtml();
+  addcss();
+  addjs();
+  addreact();
+  addbootsrap();
+}
+
+// Listen for scroll events and call handleScroll function
+window.addEventListener('scroll', handleScroll);
